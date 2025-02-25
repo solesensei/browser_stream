2c39bcceb3f81a1c087ae95b4528eeeb4ea855b5
solesensei <dimargfd@gmail.com>
2025-02-25 21:42:55 +0100

    Fix subtitle file renaming to use correct backup suffix format

--- browser_stream/helpers.py
+++ browser_stream/helpers.py
@@ -549,7 +549,7 @@ class Ffmpeg:
         )
         # rename temp file
         utils.move_file(
-            subtitle_file, subtitle_file.with_suffix(f".bak{subtitle_file.suffix}")
+            subtitle_file, subtitle_file.with_suffix(f".bakup{subtitle_file.suffix}")
         )
         utils.move_file(temp_output_file, subtitle_file)
 
